69088d1683096852cb1338b70364cc951bbb12cd
Ahmed Elzaria <aelzaria@gmail.com>
2024-01-22 21:45:53 -0500

    Refactored Loading Maze into 2d array

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
@@ -16,65 +16,46 @@ public class Maze {
     private final Tile[][] maze_grid;
 
     public Maze(File maze_file) {
-
-        int[] dim = maze_dim(maze_file);
-        this.maze_grid = new Tile[dim[0]][dim[1]];
-        load_maze(this.maze_grid, maze_file);
-
-    }
-
-    private void load_maze(Tile[][] maze_grid, File maze_file) {
-
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(maze_file));
-
-            String line;
-            int rows = maze_grid.length;
-            int num_cols = maze_grid[0].length;
-
-            for (int row = 0; row < rows; row++) {
-                line = reader.readLine();
-                for (int col = 0; col < num_cols; col++) {
-
-                    maze_grid[row][col] = (col < num_cols ? (line.charAt(col) == '#' ? Tile.WALL : Tile.EMPTY) : Tile.EMPTY);
-
-                }
-            }
-
-            reader.close();
-
-        } catch (Exception e) {
-            System.err.print("ERROR LOADING MATRIX\n");
-            System.exit(1);
-        }
-
-    }
-
-    private int[] maze_dim(File maze_file) {
-        int[] dimensions = new int[2];
         
+        this.maze_grid = load_maze(maze_file);
+
+    }
+
+    private Tile[][] load_maze(File maze_file) {
+
         try {
+            
             BufferedReader reader = new BufferedReader(new FileReader(maze_file));
             int rows = 1, cols = reader.readLine().length();
             
             while (reader.readLine() != null) { rows++; }
-            
-            dimensions[0] = rows; dimensions[1] = cols;
 
             reader.close();
 
+            BufferedReader reader2 = new BufferedReader(new FileReader(maze_file));
+
+            String line;
+            Tile[][] maze = new Tile[rows][cols];
+
+            for (int row = 0; row < rows; row++) {
+                line = reader2.readLine();
+                for (int col = 0; col < cols; col++) {
+                    maze[row][col] = (col < cols ? (line.charAt(col) == '#' ? Tile.WALL : Tile.EMPTY) : Tile.EMPTY);
+                }
+            }
+
+            reader2.close();
+
+            return maze;
+
         } catch (Exception e) {
-            System.err.print("ERROR COMPUTING MATRIX DIMENSIONS\n");
+            System.err.print("ERROR LOADING MATRIX\n");
             System.exit(1);
+            return null;
         }
 
-        return dimensions;
-    }
+        
 
-
-    public MazePath path() {
-        MazeExplorer maze_exp = new RightHand();
-        return maze_exp.find_path(this);
     }
 
     public void print_maze() {
@@ -84,6 +65,11 @@ public class Maze {
             }
             System.out.println();
         }
+    }
+
+    public MazePath path() {
+        MazeExplorer maze_exp = new RightHand();
+        return maze_exp.find_path(this);
     }
 
     private int start() { return 10; }
