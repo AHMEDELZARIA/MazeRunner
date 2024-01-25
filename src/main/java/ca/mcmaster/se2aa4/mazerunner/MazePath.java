36e75991a16789210d12f09ec9fde73f3ea805cf
Ahmed Elzaria <aelzaria@gmail.com>
2024-01-24 23:33:13 -0500

    Factorized Path Output Implemented and Working (Close #3)

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/MazePath.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/MazePath.java
@@ -11,6 +11,37 @@ public class MazePath {
     }
 
     @Override
-    public String toString() { return directions; }
+    public String toString() { return factorized_path(directions); }
+
+    private String factorized_path(String path) {
+        
+        path = path.replaceAll("\\s", "");
+        String fact_path = "";
+        int occur = 0;
+        
+        int i = 0, j;
+        while (i < path.length()) {
+            
+            for (j = i; j < path.length(); j++) {
+                if (path.charAt(j) == path.charAt(i)) {
+                    occur++;
+                } else {
+                    break;
+                }
+            }
+            
+            if (occur > 1) {
+                fact_path += String.valueOf(occur) + String.valueOf(path.charAt(i)) + " ";
+            } else {
+                fact_path += String.valueOf(path.charAt(i)) + " ";
+            }
+            
+            i = j;
+            occur = 0;
+        }
+        
+        return fact_path;
+    
+    }
 
 }
